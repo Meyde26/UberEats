d433796e68ec68da2a8b284db453b6c372694080
Mehdi KECHICHE <mehdi.kechiche26@gmail.com>
2023-09-28 09:17:34 +0200

    Feat: first commit

---
+++ src/page/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <RestaurantRow/>
+  </div>
+</template>
+
+<script>
+// IMPORT
+// import  BDD from '../Bdd.js'
+// COMPONENT
+import RestaurantRow from '../components/RestaurantRow.vue';
+export default {
+    name: 'Home',
+    components: {
+        RestaurantRow
+    },
+}
+</script>
+
+<style>
+
+</style>
