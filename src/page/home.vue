477b0db2ff4e0554c1e5071b79f4f6b4f077996f
Mehdi KECHICHE <mehdi.kechiche26@gmail.com>
2023-10-02 11:39:10 +0200

    feat: add watch & RegExp

--- src/page/home.vue
+++ src/page/home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div class="header">
       <img src="https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/ee037401cb5d31b23cf780808ee4ec1f.svg" alt="Logo UberEats">
-      <input type="text" placeholder="Cherchez votre bonheur !">
+      <input v-model="user_search_restaurant" type="text" placeholder="Cherchez votre bonheur !">
     </div>
     <div class="bannier"></div>
     <RestaurantRow v-for="(data, i) in data_restaurant" :key="i" :three_restaurant="data"/>
@@ -12,7 +12,7 @@
 <script>
 // IMPORT
  import  BDD from '../BDD'
- import {onMounted, ref } from 'vue'
+ import {onMounted, ref, watch } from 'vue'
 // COMPONENT
 import RestaurantRow from '../components/RestaurantRow.vue';
 
@@ -32,7 +32,7 @@ export default {
       }
 
       let data_restaurant = ref([]);
-
+      let all_restaurant = [];
       const makeDataRestaurant = () => {
 
         let three_restaurant = [];
@@ -40,7 +40,8 @@ export default {
 
         for (const restaurant of BDD) {
           const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time);
-
+          // make all restaurant array
+          all_restaurant.push(new_restaurant);
           
           
           if(three_restaurant.length === 2) {
@@ -53,10 +54,22 @@ export default {
         }
       }
 
+      // User search restaurant
+      let user_search_restaurant = ref('');
+
+      watch(user_search_restaurant, new_Value => {
+
+        let regex = RegExp(new_Value);
+
+       let search_restaurant = all_restaurant.filter(restaurant => regex.test(restaurant.name));
+       console.log(search_restaurant);
+      })
+
       onMounted(makeDataRestaurant);
       // Le return
       return {
         data_restaurant,
+        user_search_restaurant
       }
     },
 }
